f4441d83d0cbb96ceb64019ac6ddc1ea16729a18
baquer <baquer@github.com>
2018-03-05 18:19:08 +0530

    Initial Commit

---
+++ GoogleCastSender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleCastSender
+//
+//  Created by Syed on 05/03/18.
+//  Copyright © 2018 Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
